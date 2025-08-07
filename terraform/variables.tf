2334d69516a92c9aaa6a3e2cbec71f4ca34c8a77
J Goossens <jgoos@users.noreply.github.com>
2025-08-08 00:53:32 +0200

    Remove protocol prefixes from LDAP server defaults

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,21 +10,21 @@ variable "te_user_email" {
 }
 
 variable "ldap_servers" {
-  description = "A list of LDAP servers to monitor."
+  description = "A list of LDAP servers to monitor. `hostname` should exclude any protocol prefix."
   type = list(object({
     name     = string
-    hostname = string
+    hostname = string # Hostname without protocol prefix
     port     = number
   }))
   default = [
     {
       name     = "Primary LDAP Server (DC1)"
-      hostname = "ldaps://dc1.example.com"
+      hostname = "dc1.example.com"
       port     = 636
     },
     {
       name     = "Secondary LDAP Server (DC2)"
-      hostname = "ldaps://dc2.example.com"
+      hostname = "dc2.example.com"
       port     = 636
     }
   ]
